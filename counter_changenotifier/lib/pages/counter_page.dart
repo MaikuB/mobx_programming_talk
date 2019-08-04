4a17a41e4ce5c55dd7314f05276841b781884384
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2019-08-04 16:40:45 +1000

    optimise counter changenotifier sample

--- counter_changenotifier/lib/pages/counter_page.dart
+++ counter_changenotifier/lib/pages/counter_page.dart
@@ -5,37 +5,36 @@ import '../view_models/counter_view_model.dart';
 class CounterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<CounterViewModel>(
-      builder: (_, counterBloc, __) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Counter'),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'You have pushed the button this many times:',
-                ),
-                Consumer<CounterViewModel>(
-                  builder: (context, viewmodel, _) {
-                    return Text(
-                      '${viewmodel.counter}',
-                      style: Theme.of(context).textTheme.display1,
-                    );
-                  },
-                ),
-              ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
             ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: counterBloc.increment,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-        );
-      },
+            Consumer<CounterViewModel>(
+              builder: (context, viewModel, _) {
+                debugPrint('built counter text widget');
+                return Text(
+                  '${viewModel.counter}',
+                  style: Theme.of(context).textTheme.display1,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Provider.of<CounterViewModel>(context).increment();
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
--- counter_changenotifier/lib/view_models/counter_view_model.dart
+++ counter_changenotifier/lib/view_models/counter_view_model.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 class CounterViewModel extends ChangeNotifier {
   int counter = 0;
+  // int x = 0;
 
   void increment() {
     counter++;
+    // x++;
     notifyListeners();
   }
 }
